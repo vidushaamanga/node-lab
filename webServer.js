10d4cfb22b71f76d487c8cab184e561570334d0b
Vidhu <viduhettiarachchi@gmail.com>
2026-02-23 22:10:24 +0530

    Add web server example

---
+++ webServer.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8081);
+
+console.log('Server running at http://localhost:8081/');
